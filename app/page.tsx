fb7bb46bc0c61c7d90ef691bccf74e52ec52bce5
Tsk <92300520+TskQ@users.noreply.github.com>
2025-04-25 09:21:47 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
           onMouseEnter={() => setHoverEmoji(true)}
           onMouseLeave={() => setHoverEmoji(false)}
         >
-          {hoverEmoji ? "☆*: .｡. o(≧▽≦)o .｡.:*☆" : "(/≧▽≦)/"}
+          {hoverEmoji ? "AC2611" : "(/≧▽≦)/"}
         </span>
       </div>
       <DiscordPresence userId="1002839537644482611" />
